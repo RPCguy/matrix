5f201f423ad21fd0b4d7cc8d7ff3cf257cbdd764
RPCguy <pippin.chell@gmail.com>
2024-10-16 15:54:46 +0000

    Add files via upload
    hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ matrix.py
@@ -0,0 +1,21 @@
+# create a 2d list
+d2_list = [[1,2,3],[4,5,6],[7,8,9]]
+print(d2_list)
+
+# finding lengh of 2d list row
+row = len(d2_list)
+print("The total rows are", row)
+
+# finding lengh of 2d list column
+col = len(d2_list[0])
+print("The total columns are", col)
+
+# accesing elements in a 2d list
+hiiiiiiii_item = d2_list[1][1]
+print("The special number is...  " , hiiiiiiii_item)
+
+# adding values in an empty metrix
+print("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+matrix = []
+rows = int(input("How Many Rows? "))
+cols = int(input("How Many Coloumns? "))
